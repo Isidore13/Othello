3a4311aedf7c83251a918f42b3583862b2e344c0
Horigon29 <quemenernic@gmail.com>
2024-12-11 15:06:15 +0100

    Update Methode.java

--- Methode.java
+++ Methode.java
@@ -15,6 +15,28 @@ public class Methode {
         
 
     }
+    
+    public static int[] saisieutilisateur() {
+        Scanner scanner = new Scanner(System.in);
+        String positionpion;
+        int[] positionfinal=new int[2];
+        boolean verification=false;
+
+        do{
+            System.out.print("Saisisser l'emplacement ou placer le pion : ");
+            positionpion = scanner.nextLine();
+            if(positionpion.length()!=2 || positionpion.charAt(0)<'A' || positionpion.charAt(0)>'H' || positionpion.charAt(1)<'1' || positionpion.charAt(1)>'8'){
+                System.out.println("Erreur saisisser une valeur entre (A et H) en majuscule et une valeur entre (1 et 8) par exemple E5");
+                verification=false;
+            }
+            else {
+                verification=true;
+            }
+        }while(verification==false);
+        positionfinal[0]=((int)positionpion.charAt(0))-64; // 64 car 'A' - 64 egale 1 pour avoir les coordonnee
+        positionfinal[1]=((int)positionpion.charAt(1))-48; //48 car '1' egale 49 donc si l'on soustrait 48 '1' egale 1
+        return positionfinal;
+    }
 
 
 
