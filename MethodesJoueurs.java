56c1db0c3fb0513c3ee6c81d47dd7581e7d1080e
Isidore <meilleurisidore13@gmail.com>
2024-12-13 16:36:00 +0100

    Update MethodesJoueurs.java

--- MethodesJoueurs.java
+++ MethodesJoueurs.java
@@ -1,1 +1,11 @@
+import java.util.Scanner;
 
+public class MethodesJoueurs {
+
+    public static void afficherScore(String[] pseudo, int[] score){
+
+    }
+
+    public static void modifierPseudo(){}
+
+}
