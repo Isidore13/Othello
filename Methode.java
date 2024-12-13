d3cdef356d50f6a2159e1f9c1da00e811b3a47a8
Horigon29 <quemenernic@gmail.com>
2024-12-13 13:52:55 +0100

    Update Methode.java

--- Methode.java
+++ Methode.java
@@ -1,60 +1,56 @@
 import java.util.Scanner;
 
-public class Methodes {
+public class Methode {
+    public static void main(String[] args) {
+        char[][] plateau = creationPlateau();
+        saisieutilisateur(plateau);
+    }
 
-    public static char[][] creationPlateau(char[][] plateau) {
+
+    public static char[][] creationPlateau() {
         //plateau[y][x]
-        char[][] plateau = {{'\0','A','B','C','D','E','F','G','H'},
-                            {'1','*','*','*','*','*','*','*','*'},
-                            {'2','*','*','*','*','*','*','*','*'},
-                            {'3','*','*','*','*','*','*','*','*'},
-                            {'4','*','*','*','B','W','*','*','*'},
-                            {'5','*','*','*','W','B','*','*','*'},
-                            {'6','*','*','*','*','*','*','*','*'},
-                            {'7','*','*','*','*','*','*','*','*'},
-                            {'8','*','*','*','*','*','*','*','*'}};
+        char[][] plateau = {{'\0', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'},
+                {'1', '*', '*', '*', '*', '*', '*', '*', '*'},
+                {'2', '*', '*', '*', '*', '*', '*', '*', '*'},
+                {'3', '*', '*', '*', '*', '*', '*', '*', '*'},
+                {'4', '*', '*', '*', 'B', 'W', '*', '*', '*'},
+                {'5', '*', '*', '*', 'W', 'B', '*', '*', '*'},
+                {'6', '*', '*', '*', '*', '*', '*', '*', '*'},
+                {'7', '*', '*', '*', '*', '*', '*', '*', '*'},
+                {'8', '*', '*', '*', '*', '*', '*', '*', '*'}};
 
         return plateau;
-        
+
 
     }
-    
-    public static int[] saisieutilisateur(char[][] plateau) {
-        
-        Scanner scanner = new Scanner(System.in);
-        
-        String positionPion;
-        int[] positionFinale=new int[2];
-        boolean verification=false;
 
-        do{
+    public static int[] saisieutilisateur(char[][] plateau) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String positionPion;
+        int[] positionFinale = new int[2];
+        boolean verification = false;
+
+        do {
             System.out.print("Saisissez l'emplacement où placer le pion : ");
             positionPion = scanner.nextLine();
-            
-            if(positionPion.length()!=2 || positionPion.charAt(0)<'A' || positionPion.charAt(0)>'H' || positionPion.charAt(1)<'1' || PositionPion.charAt(1)>'8'){
+
+            if (positionPion.length() != 2 || positionPion.charAt(0) < 'A' || positionPion.charAt(0) > 'H' || positionPion.charAt(1) < '1' || positionPion.charAt(1) > '8') {
                 System.out.println("Erreur saisisser une valeur entre (A et H) en majuscule et une valeur entre (1 et 8) par exemple E5");
-            }
-                
-            else{
-                verification=true;
-                Pion.charpositionFinale[0]=((int)positionAt(0))-64; // 64 car 'A' - 64 egale 1 pour avoir les coordonnee
-                positionFinale[1]=((int)positionPion.charAt(1))-48; //48 car '1' egale 49 donc si l'on soustrait 48 '1' egale 1
+            } else {
+                verification = true;
+                positionFinale[0] = ((int) positionPion.charAt(0)) - 64; // 64 car 'A' - 64 egale 1 pour avoir les coordonnee
+                positionFinale[1] = ((int) positionPion.charAt(1)) - 48; //48 car '1' egale 49 donc si l'on soustrait 48 '1' egale 1
                 //Verifie si un pion existe à l'emplacement choisi
-                if (plateau[positionFinale[0]][positionFinale[1]]!='*'){
-                    verification=false;
-                    System.out.println("Il y à déjà un pion ici.")
+                if (plateau[positionFinale[0]][positionFinale[1]] != '*') {
+                    verification = false;
+                    System.out.println("Il y à déjà un pion ici.");
                 }
-                
+
             }
-            
-        }
-        
-        while(verification==false);
-        
-        Pion.charpositionFinale[0]=((int)positionAt(0))-64; // 64 car 'A' - 64 egale 1 pour avoir les coordonnee
-        positionFinale[1]=((int)positionPion.charAt(1))-48; //48 car '1' egale 49 donc si l'on soustrait 48 '1' egale 1
-        
+
+        } while (verification == false);
         return positionFinale;
     }
-
 }
