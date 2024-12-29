e3ff451b79e69279ba882aa354808c4b8ab3e3d2
Horigon29 <quemenernic@gmail.com>
2024-12-29 17:41:33 +0100

    Update Methode.java

--- Methode.java
+++ Methode.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Methode {
@@ -28,10 +29,9 @@ public class Methode {
         boolean verification = false;
 
         do {
-            if(tourjoueur=='r'){
+            if (tourjoueur == 'r') {
                 System.out.print("C'est au tour de " + pseudo[0] + " de saisir l'emplacement où placer le pion (pion rouge): ");
-            }
-            else{
+            } else {
                 System.out.print("C'est au tour de " + pseudo[1] + " de saisir l'emplacement où placer le pion (pion bleu): ");
             }
 
@@ -68,7 +68,6 @@ public class Methode {
             plateau[coordonées[0]][coordonées[1]] = 'b';
         }
 
-
     }
 
     // Cette fonction affiche l'état du plateau
@@ -103,6 +102,7 @@ public class Methode {
         return true;
     }
 
+    //Cette fonction vérifie si un pion ennemi est bien encadrer si c'est le cas renvoie true et si mange est à true remplace le pion ennemi en pion allié sinon renvoie false
     public static boolean pionEncadrerEtPeutManger(char[][] plateau, int[] coordonnee, char tourjoueur, boolean mange) {
         boolean verifmanger = false;
         boolean encadrement = false;
@@ -117,7 +117,7 @@ public class Methode {
                         pionVerifier[0] += y;
                         pionVerifier[1] += x;
                     }
-                    // verifie si bien pion alié
+                    // verifie si bien pion allié
                     if (plateau[pionVerifier[0] + y][pionVerifier[1] + x] == tourjoueur) {
                         encadrement = true;
                     }
@@ -147,15 +147,43 @@ public class Methode {
             for (int x = 1; x < 9; x++) {
                 coordonnee[0] = y;
                 coordonnee[1] = x;
-                if (plateau[y][x]=='*'&& pionEncadrerEtPeutManger(plateau, coordonnee, tourjoueur, false)) {
+                if (plateau[y][x] == '*' && pionEncadrerEtPeutManger(plateau, coordonnee, tourjoueur, false)) {
                     peutJouer = true;
-                    System.out.print("["+((char)(x+64))+","+y+"]");
+                }
+            }
+        }
+        return peutJouer;
+
+    }
+
+    public static ArrayList<int[]> listePositionPossibleDeJouer(char[][] plateau, char tourjoueur){
+        ArrayList<int[]> positionPossibleDeJouer = new ArrayList<int[]>();
+        for (int y = 1; y < 9; y++) {
+            for (int x = 1; x < 9; x++) {
+                int[] coordonnee = new int[2];
+                coordonnee[0] = y;
+                coordonnee[1] = x;
+                if (plateau[y][x] == '*' && pionEncadrerEtPeutManger(plateau, coordonnee, tourjoueur, false)) {
+                    positionPossibleDeJouer.addLast(coordonnee);
+                }
+            }
+        }
+        return positionPossibleDeJouer;
+    }
+
+    public static void afficheOuPeuxJouer(char[][] plateau, char tourjoueur) {
+        int[] coordonnee = new int[2];
+        System.out.print("Vous pouvez jouer en :");
+        for (int y = 1; y < 9; y++) {
+            for (int x = 1; x < 9; x++) {
+                coordonnee[0] = y;
+                coordonnee[1] = x;
+                if (plateau[y][x] == '*' && pionEncadrerEtPeutManger(plateau, coordonnee, tourjoueur, false)) {
+                    System.out.print(" " + ((char) (x + 64)) + y);
                 }
             }
         }
         System.out.println();
-        return peutJouer;
-
     }
 
     public static void jeu(String[] pseudo, int[] score) {
@@ -164,52 +192,115 @@ public class Methode {
         boolean impossibleDeJouer = false;
         while (!plateauPlein(plateau) && !impossibleDeJouer) {
             afficherplateau(plateau);
+            afficheOuPeuxJouer(plateau, tourJoueur);
             placementPion(plateau, saisieUtilisateur(plateau, tourJoueur, pseudo), tourJoueur);
             //verifie si le joueur rouge peut jouer sinon passe sont tour
             if (tourJoueur == 'r' && verifSiPeutJouer(plateau, 'b')) {
                 tourJoueur = 'b';
             }
             //verifie si le joueur bleu peut jouer s'il ne peux pas jouer il passe son tour
-            else if (verifSiPeutJouer(plateau, 'r')){
+            else if (verifSiPeutJouer(plateau, 'r')) {
                 tourJoueur = 'r';
-            }
-            else if (!verifSiPeutJouer(plateau, 'r') && !verifSiPeutJouer(plateau, 'b')) {
+            } else if (!verifSiPeutJouer(plateau, 'r') && !verifSiPeutJouer(plateau, 'b')) {
                 impossibleDeJouer = true;
             }
 
         }
         afficherplateau(plateau);
-        calculVictoire(plateau);
+        afficheGagnant(plateau);
 
         if (calculVictoire(plateau) != 2) {
             score[calculVictoire(plateau)] += 1;
         }
     }
 
+    public static void jeuContreIA(String[] pseudo) {
+        char tourJoueur = 'r';
+        int[] positionIA = new int[2];
+        char[][] plateau = creationPlateau();
+        boolean impossibleDeJouer = false;
+        while (!plateauPlein(plateau) && !impossibleDeJouer) {
+            if (tourJoueur == 'r') {
+                afficherplateau(plateau);
+                afficheOuPeuxJouer(plateau, tourJoueur);
+                //affiche seulement si le bot à déjà jouer
+                if(positionIA[0]!=0) {
+                    System.out.println("Le bot à jouer en " + (char) (positionIA[1] + 64) + positionIA[0]);
+                }
+                placementPion(plateau, saisieUtilisateur(plateau, tourJoueur, pseudo), tourJoueur);
+
+            } else {
+                positionIA = positionJouerParIA(plateau,listePositionPossibleDeJouer(plateau, tourJoueur));
+            }
+
+            //verifie si le joueur rouge peut jouer sinon passe sont tour
+            if (tourJoueur == 'r' && verifSiPeutJouer(plateau, 'b')) {
+                tourJoueur = 'b';
+            }
+            //verifies si le joueur bleu peut jouer s'il ne peux pas jouer il passe son tour
+            else if (verifSiPeutJouer(plateau, 'r')) {
+                tourJoueur = 'r';
+            } else if (!verifSiPeutJouer(plateau, 'r') && !verifSiPeutJouer(plateau, 'b')) {
+                impossibleDeJouer = true;
+            }
+
+        }
+        afficherplateau(plateau);
+        afficheGagnant(plateau);
+    }
+
+    public static int[] positionJouerParIA(char[][] plateau, ArrayList<int[]> position) {
+        int choixPosition = (int)Math.random() * position.size();
+        plateau[position.get(choixPosition)[0]][position.get(choixPosition)[1]] = 'b';
+        pionEncadrerEtPeutManger(plateau, position.get(choixPosition),'b',true);
+        return position.get(choixPosition);
+    }
+
     public static int calculVictoire(char[][] plateau) {
-        int pionBlanc = 0;
-        int pionNoir = 0;
+        int pionRouge = 0;
+        int pionBleu = 0;
         int indiceScoreGagnant;
 
         for (int y = 1; y < 9; y++) {
             for (int x = 1; x < 9; x++) {
                 if (plateau[y][x] == 'r') {
-                    pionBlanc++;
-                } else
-                    pionNoir++;
+                    pionRouge++;
+                } else if (plateau[y][x] == 'b') {
+                    pionBleu++;
+                }
             }
         }
 
-        if (pionBlanc > pionNoir) {
-            System.out.println("Victoire des rouges");
+        if (pionRouge > pionBleu) {
             indiceScoreGagnant = 0;
-        } else if (pionNoir > pionBlanc) {
-            System.out.println("Victoire des bleu");
+        } else if (pionBleu > pionRouge) {
             indiceScoreGagnant = 1;
         } else {
-            System.out.println("Il y a égalité");
             indiceScoreGagnant = 2;
         }
         return indiceScoreGagnant;
     }
+
+    public static void afficheGagnant(char[][] plateau) {
+        int pionRouge = 0;
+        int pionBleu = 0;
+        for (int y = 1; y < 9; y++) {
+            for (int x = 1; x < 9; x++) {
+                if (plateau[y][x] == 'r') {
+                    pionRouge++;
+                } else if (plateau[y][x] == 'b') {
+                    pionBleu++;
+                }
+            }
+        }
+
+        if (pionRouge > pionBleu) {
+            System.out.println("Victoire des rouge avec " + pionRouge + " points contre " + pionBleu + " points pour les bleu");
+        } else if (pionBleu > pionRouge) {
+            System.out.println("Victoire des bleu avec " + pionBleu + " points contre " + pionRouge + " points pour les rouge");
+        } else {
+            System.out.println("Il y a égalité");
+        }
+    }
+
 }
